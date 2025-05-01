655785499920a6018f48b7a65ae5729fed2db8da
Darshan Athadakar <145689299+darshan-a28@users.noreply.github.com>
2025-05-01 14:34:56 +0530

    Update setup.sql

--- HotelReservationSystem/SQL_Scripts /setup.sql
+++ HotelReservationSystem/SQL_Scripts /setup.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE `hotel_mgmt`.`reservations` (
+  `reservation_id` INT NOT NULL AUTO_INCREMENT,
+  `guest_name` VARCHAR(225) NOT NULL,
+  `room_number` INT NOT NULL,
+  `contact_number` VARCHAR(10) NOT NULL,
+  `reservation_date` TIMESTAMP NULL,
+  PRIMARY KEY (`reservation_id`));
